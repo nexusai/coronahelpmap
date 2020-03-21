4d53e14b7ccf3586da0d065c0d1c6a4dc089e14c
Mateo Bello Gularte <mbellogularte@gmail.com>
2020-03-21 17:49:03 +0100

    nodemailer

---
+++ CoronaHelp/public/helper.js
@@ -0,0 +1,45 @@
+
+
+    var db = firebase.firestore();
+
+
+
+    const form = document.querySelector('#input_fields');
+
+ 
+// saving data:
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('helpersPreZip').add({
+        address: form.address.value,
+        contactInfo: form.contactInfo.value,
+        firstName: form.first_name.value,
+        typeOfHelp: form.typeOfHelp.value,
+        age: form.age.value
+    })
+.then(docRef => {
+    console.log("Document written with ID: ", docRef.id);
+    console.log("You can now also access .this as expected: ", this.foo);
+    let data = {
+          id: docRef.id
+          };
+
+          
+    db.collection("helpersPreZip").doc(docRef.id)
+.set(data, {merge: true});
+    form.reset();
+
+})
+.catch(error => console.error("Error adding document: ", error))
+    //window.location.assign("https://coronahelpmap.com/");
+
+
+})
+
+
+
+
+
+
---
+++ CoronaHelp/public/index.js
@@ -0,0 +1,99 @@
+/*!
+* Markers On Map ('https://github.com/furcan/Markers-On-Map')
+* Version: 1.3.0
+* Author: Furkan MT ('https://github.com/furcan')
+* Copyright 2019 Markers On Map, MIT Licence ('https://opensource.org/licenses/MIT')*
+*/
+
+// DEMO: Markers On Map - Init and Run on
+
+
+
+var db = firebase.firestore();
+var helpers = [];
+var searcher = [];
+
+
+
+
+db.collection("helpers").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+
+       // console.log(`${doc.id} => ${doc.data()}`);
+        //console.log(doc.data().firstName);
+        var helper = new add(helperMarker(),25,doc.data().addressLat,doc.data().addressLong,doc.data().firstName,'<h3 style="text-align:center;margin:0 0 10px;">' + doc.data().firstName + ", " + doc.data().age.toString() + '</h3><p style="text-align:center; margin:0 0 10px;">' + doc.data().typeOfHelp + '</p><button style="display:table;margin:auto;padding:8px 12px;border-radius:20px;font-weight:700;background:#DE2A00;color:#fff;cursor:pointer;">' + doc.data().contactInfo + '</button>');
+        helpers.push(helper)
+
+
+    });
+            //console.log(helpers)
+
+db.collection("searcher").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+
+       // console.log(`${doc.id} => ${doc.data()}`);
+        //console.log(doc.data().firstName);
+        var search = new add(searcherMarker(),25,doc.data().addressLat,doc.data().addressLong,doc.data().firstName,'<h3 style="text-align:center;margin:0 0 10px;">' + doc.data().firstName + ", " + doc.data().age.toString() + '</h3><p style="text-align:center; margin:0 0 10px;">' + doc.data().typeOfHelp + '</p><button style="display:table;margin:auto;padding:8px 12px;border-radius:20px;font-weight:700;background:#DE2A00;color:#fff;cursor:pointer;">' + doc.data().contactInfo + '</button>');
+        searcher.push(search)
+
+
+    });
+            //console.log(helpers)
+
+            MarkersOnMap.Init({
+    googleApiKey: 'AIzaSyBDWpSKKqmaBHmKwBobTEjxToXSRk2GkPc', // this key restricted except this project
+    googlePlacesApiEnabled: true,
+    mapTypeId: 'terrain',
+    mapHeight: '500px',
+    markerOverlay: false,
+    mapZoomControl: true,
+    mapScrollWheel: false,
+    markerObjects: helpers.concat(searcher),
+});
+MarkersOnMap.Run('div#GoogleMaps');
+
+furcanTooltip('[data-toggle="tooltip"]');
+
+});
+
+});
+
+
+
+
+// DEMO: Markers On Map - Init and Run off
+function add (markerUrl,markerSize,markerLat,markerLong,markerTitle,markerContent) {
+this.markerUrl = markerUrl;
+this.markerSize = markerSize;
+this.markerLat = markerLat;
+this.markerLong = markerLong;
+this.markerTitle = markerTitle;
+this.markerContent = markerContent;
+}
+
+// DEMO: Tooltip on
+function furcanTooltip(tooltip) {
+    $('body > .tooltip').remove();
+    $(tooltip).tooltip({
+        trigger: 'hover',
+        container: 'body',
+    });
+}
+
+$(document).on('click', function () {
+    if ($('body > .tooltip').length > 0) {
+        $('body > .tooltip').remove();
+    }
+});
+// DEMO: Tooltip off
+
+// DEMO: Map Markers Title Tooltip on
+$(window).on('load', function () {
+    var tooltipTimeout = setTimeout(function () {
+        $(document).on('mouseenter', 'div#GoogleMaps', function () {
+            furcanTooltip($('div#GoogleMaps *[title]'));
+            clearTimeout(tooltipTimeout);
+        });
+    }, 1000);
+});
+// DEMO: Map Markers Title Tooltip off
